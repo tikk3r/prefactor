f8062f636e734dec33d27dffcc425060c12d1bae
Frits Sweijen <fritssweijen@hotmail.com>
2019-03-06 10:42:44 +0100

    Fix target updating script.

--- scripts/update_tgt_field.py
+++ scripts/update_tgt_field.py
@@ -3,11 +3,30 @@ import argparse
 
 import casacore.tables as ct
 
-def main(msname, newtarget):
-    ms = msname.rstrip('/')
-    tab = ct.table(ms + '::OBSERVATION', readonly=False)
-    tab.putcell('LOFAR_TARGET', 0, newtarget)
-    tab.close()
+def input2strlist_nomapfile(invar):
+   """ 
+   from bin/download_IONEX.py
+   give the list of MSs from the list provided as a string
+   """
+   str_list = None
+   if type(invar) is str:
+       if invar.startswith('[') and invar.endswith(']'):
+           str_list = [f.strip(' \'\"') for f in invar.strip('[]').split(',')]
+       else:
+           str_list = [invar.strip(' \'\"')]
+   elif type(invar) is list:
+       str_list = [str(f).strip(' \'\"') for f in invar]
+   else:
+       raise TypeError('input2strlist: Type '+str(type(invar))+' unknown!')
+    return str_list
+
+def main(mses, newtarget):
+    mslist = input2strlist_nomapfile(mses)
+    for msname in mslist:
+        ms = msname.rstrip('/')
+        tab = ct.table(ms + '::OBSERVATION', readonly=False)
+        tab.putcell('LOFAR_TARGET', 0, newtarget)
+        tab.close()
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
